26bf3df63441a45db03fcca15b065a321bdbeb32
Edigleysson Silva <edigleyssonsilva@gmail.com>
2021-01-23 15:39:17 -0300

    feat: added airships

--- src/app/pages/home-map/home-map.component.ts
+++ src/app/pages/home-map/home-map.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Airship } from 'src/models/airship.interface';
 
 
 declare var google: any;
@@ -12,6 +13,57 @@ export class HomeMapComponent implements OnInit {
 
   isLoading: boolean = false;
   step: number = 0;
+
+  airships: Airship[] = [
+    {
+      name: 'Airbus H125',
+      maximumSpeed: 287,
+      fasCruiseSpeed: 260,
+      range: 640,
+      endurance: 15600
+    },
+
+    {
+      name: 'Airbus H130',
+      maximumSpeed: 287,
+      fasCruiseSpeed: 235,
+      range: 610,
+      endurance: 14400
+    },
+
+    {
+      name: 'Bell 505',
+      maximumSpeed: 270,
+      fasCruiseSpeed: 231,
+      range: 566,
+      endurance: 14400
+    },
+
+
+    {
+      name: 'Bell 407',
+      maximumSpeed: 270,
+      fasCruiseSpeed: 246,
+      range: 624,
+      endurance: 14400
+    },
+
+    {
+      name: 'Robinson R66 - Turbine',
+      maximumSpeed: 260,
+      fasCruiseSpeed: 200,
+      range: 630,
+      endurance: 10800
+    },
+
+    {
+      name: 'Robinson R44 - Raven II',
+      maximumSpeed: 240,
+      fasCruiseSpeed: 200,
+      range: 560,
+      endurance: 10800
+    }
+  ];
 
   constructor() { }
 
---
+++ src/models/airship.interface.ts
@@ -0,0 +1,7 @@
+export interface Airship {
+  name: string;
+  maximumSpeed: number;
+  fasCruiseSpeed: number;
+  range: number; // autonomia com um tanque padrão
+  endurance: number; // milissecond
+}
